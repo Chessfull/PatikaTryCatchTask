9e133e36df3ab2b6b8e0179f4bd11bc49de77171
Mert Topcu <merttopcu.dev@gmail.com>
2024-08-11 16:29:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaTryCatchTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool formatValidCheck = false; // -> Key of loop
+            while (!formatValidCheck) 
+            {
+                try
+                {
+                    Console.Write("Your number:");
+                    int userNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(Math.Pow(userNumber, 2)); // -> Taking square of number
+                    formatValidCheck = true; // -> Open the key
+                }
+                catch (FormatException) // -> Format exception control
+                {
+                    Console.WriteLine("Wrong format. Please go for number!");
+
+                }
+
+                catch (Exception ex) // -> Any exception control
+                {
+                    Console.WriteLine("Wrong input process." + ex.Message);
+
+                }
+            }
+        }
+    }
+}
